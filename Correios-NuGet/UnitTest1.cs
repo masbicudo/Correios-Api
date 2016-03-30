1f54a6c4dd567b2558a4ec1a8ce98805a6ee7897
masbicudo <masbicudo@gmail.com>
2016-03-30 03:28:19 -0300

    Project to test NuGet

---
+++ Correios-NuGet/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Correios;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Correios_NuGet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new CorreiosApi();
+            var task = service.consultaCEPAsync("24710480");
+            var result = task.Result;
+            var bairro = result.@return.bairro;
+        }
+    }
+}
